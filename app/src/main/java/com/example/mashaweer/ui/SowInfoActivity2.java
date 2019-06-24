3253b23b4f76ed8bb67c8d527b1084d4b9dd88ad
middle <middleeastapp67@gmail.com>
2019-06-24 10:46:29 +0200

    6

---
+++ app/src/main/java/com/example/mashaweer/ui/SowInfoActivity2.java
@@ -0,0 +1,104 @@
+package com.example.mashaweer.ui;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mashaweer.R;
+
+public class SowInfoActivity2 extends AppCompatActivity {
+    TextView nameTv, addressTv, phoneTv, mailTv;
+    private String phone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sow_info2);
+        nameTv = findViewById(R.id.show_Name2);
+        addressTv = findViewById(R.id.show_address2);
+        phoneTv = findViewById(R.id.show_Phone2);
+        mailTv = findViewById(R.id.show_Email2);
+
+
+        getIntentData();
+
+
+        phoneTv.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @java.lang.Override
+            public void onClick(View view) {
+
+                onCallBtnClick();
+
+            }
+        });
+
+    }
+
+    private void getIntentData() {
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String address = intent.getStringExtra("address");
+        phone = intent.getStringExtra("phone");
+        String mail = intent.getStringExtra("mail");
+
+        mailTv.setText(mail);
+        nameTv.setText(name);
+        addressTv.setText(address);
+        phoneTv.setText(phone);
+    }
+
+
+    private void onCallBtnClick(){
+        if (Build.VERSION.SDK_INT < 23) {
+            phoneCall();
+        }else {
+
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+
+                phoneCall();
+            }else {
+                final String[] PERMISSIONS_STORAGE = {Manifest.permission.CALL_PHONE};
+                //Asking request Permissions
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, 9);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        boolean permissionGranted = false;
+        switch(requestCode){
+            case 9:
+                permissionGranted = grantResults[0]== PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if(permissionGranted){
+            phoneCall();
+        }else {
+            Toast.makeText(this, "You don't assign permission.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void phoneCall(){
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + phone));
+            startActivity(callIntent);
+        }else{
+            Toast.makeText(this, "You don't assign permission.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
