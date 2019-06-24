1016a4d1755d6391d96ee815cbdc8c52b92c1c29
middle <middleeastapp67@gmail.com>
2019-06-24 18:38:36 +0200

    6

---
+++ app/src/main/java/com/example/mashaweer/ui/StartSplash.java
@@ -0,0 +1,35 @@
+package com.example.mashaweer.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+import com.example.mashaweer.R;
+
+public class StartSplash extends AppCompatActivity {
+
+    private static final long SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_splash);
+
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(StartSplash.this, SplashActivity.class);
+                StartSplash.this.startActivity(mainIntent);
+                StartSplash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    }
+
