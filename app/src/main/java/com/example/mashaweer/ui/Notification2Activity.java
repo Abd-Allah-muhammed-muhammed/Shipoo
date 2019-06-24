9d772f29343e0eaa45bca36f24503c2f6500a53a
middle <middleeastapp67@gmail.com>
2019-06-24 12:40:07 +0200

    6

--- app/src/main/java/com/example/mashaweer/ui/Notification2Activity.java
+++ app/src/main/java/com/example/mashaweer/ui/Notification2Activity.java
@@ -6,17 +6,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.mashaweer.R;
 import com.example.mashaweer.adapter.AdapterAplayAcceptNoti;
-import com.example.mashaweer.adapter.AdapterNotificatios;
 import com.example.mashaweer.helper.SharedPreferencesManger;
-import com.example.mashaweer.model.AplayAcscept;
 import com.example.mashaweer.model.ServiceAccept;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
